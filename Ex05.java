03502b9a9fef05a3f66467b260b71637cdf95e11
Lucas Bertuol <151688160+bertua@users.noreply.github.com>
2024-02-28 22:47:09 -0300

    Update Ex05.java

--- Ex05.java
+++ Ex05.java
@@ -1,66 +1,47 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Ex05 {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        double[] matematica = new double[4];
-        double[] portugues = new double[4];
-        double[] fisica = new double[4];
-        double[] quimica = new double[4];
-        double[] sociologia = new double[4];
+        ArrayList<Double> matematica = new ArrayList<>();
+        ArrayList<Double> portugues = new ArrayList<>();
+        ArrayList<Double> fisica = new ArrayList<>();
+        ArrayList<Double> quimica = new ArrayList<>();
+        ArrayList<Double> sociologia = new ArrayList<>();
         
         String[] materias = {"Matematica", "Portugues", "Fisica", "Quimica", "Sociologia"};
         
         for(int i = 0; i < 5; i++){
             System.out.println("Notas dispciplina: " + materias[i]);
-            if(i == 0){
-                for(int j = 1; j <= 4; j++){
-                    System.out.print("Nota " + j + ": ");
-                    double nota = input.nextDouble();
-                    matematica[j-1] = nota;
+            for(int j = 1; j < 5; j++){
+                System.out.print("Nota " + j + ": ");
+                double nota = input.nextDouble();
+                if(i == 0){
+                    matematica.add(nota);
+                } else if(i == 1){
+                    portugues.add(nota);
+                } else if(i == 2){
+                    fisica.add(nota);
+                } else if(i == 3){
+                    quimica.add(nota);
+                } else if(i == 4){
+                    sociologia.add(nota);
                 }
-                System.out.println(media(matematica));
             }
-            else if(i == 1){
-                for(int j = 1; j <= 4; j++){
-                    System.out.print("Nota " + j + ": ");
-                    double nota = input.nextDouble();
-                    portugues[j-1] = nota;
-                }
-                System.out.println(media(portugues));
-            }
-            else if(i == 2){
-                for(int j = 1; j <= 4; j++){
-                    System.out.print("Nota " + j + ": ");
-                    double nota = input.nextDouble();
-                    fisica[j-1] = nota;
-                }
-                System.out.println(media(fisica));
-            }
-            else if(i == 3){
-                for(int j = 1; j <= 4; j++){
-                    System.out.print("Nota " + j + ": ");
-                    double nota = input.nextDouble();
-                    quimica[j-1] = nota;
-                }
-                System.out.println(media(quimica));
-            }
-            else if(i == 4){
-                for(int j = 1; j <= 4; j++){
-                    System.out.print("Nota " + j + ": ");
-                    double nota = input.nextDouble();
-                    sociologia[j-1] = nota;
-                }
-                System.out.println(media(sociologia));
-            }
-        } 
-    }
+        }
+        System.out.println("Media Matematica: " + media(matematica));
+        System.out.println("Media Portugues: " + media(portugues));
+        System.out.println("Media Fisica: " + media(fisica));
+        System.out.println("Media Quimica: " + media(quimica));
+        System.out.println("Media Sociologia: " + media(sociologia));
+    } 
     
-    public static double media(double[] collection){
+    public static double media(ArrayList<Double> collection){
         double soma = 0;
         for(double item : collection){
             soma += item;
         }
-        return soma/collection.length;
+        return soma/collection.size();
     }
 
 }
