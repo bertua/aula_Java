bde77ae1f21c4f1309dc706abda355a2c2b59cdf
Lucas Bertuol <151688160+bertua@users.noreply.github.com>
2024-03-06 19:44:38 -0300

    Update Ex04.java

--- Ex04.java
+++ Ex04.java
@@ -9,60 +9,74 @@ public class Ex04 {
         System.out.println("5: SAIR");
         System.out.print("Escolha a operação: ");
         int ope = input.nextInt();
-        System.out.print("Primeiro número: ");
-        double a = input.nextInt();
-        System.out.print("Segundo número: ");
-        double b = input.nextInt();
 
         while(ope != 5){
             switch(ope){
                 case 1:
-                System.out.println(soma(a,b));
-                break;
+                    System.out.println(soma());
+                    break;
 
                 case 2:
-                System.out.println(menos(a,b));
-                break;
+                    System.out.println(menos());
+                    break;
 
                 case 3:
-                System.out.println(vezes(a,b));
-                break;
+                    System.out.println(vezes());
+                    break;
 
                 case 4:
-                if(b == 0){
-                    System.out.println("ERROR");
-                } else {
-                    System.out.println(div(a,b));
-                }
-                break;
+                    System.out.println(div());
+                    break;
 
                 default:
-                System.out.println("Número incorreto");
+                    System.out.println("Número incorreto");
             }
-            
+
+            System.out.println("1: Soma");
+            System.out.println("2: Subtração");
+            System.out.println("3: Multiplicação");
+            System.out.println("4: Divisão");
+            System.out.println("5: SAIR");
             System.out.print("Escolha a operação: ");
             ope = input.nextInt();
-            if(ope == 5){
-                break;
-            }
-            System.out.print("Primeiro número: ");
-            a = input.nextInt();
-            System.out.print("Segundo número: ");
-            b = input.nextInt();
        }
     }
 
-    public static double soma(double a, double b){
-        return a + b;
+    public static void soma(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Primeiro número: ");
+        double a = input.nextInt();
+        System.out.print("Segundo número: ");
+        double b = input.nextInt();
+        System.out.println("Soma: " + (a + b));
     }
-    public static double menos(double a, double b){
-        return a - b;
+    public static void menos(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Primeiro número: ");
+        double a = input.nextInt();
+        System.out.print("Segundo número: ");
+        double b = input.nextInt();
+        System.out.println("Subtração: " + (a - b));
     }
-    public static double vezes(double a, double b){
-        return a * b;
+    public static void vezes(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Primeiro número: ");
+        double a = input.nextInt();
+        System.out.print("Segundo número: ");
+        double b = input.nextInt();
+        System.out.println("Multiplicação: " + (a * b));
     }
-    public static double div(double a, double b){
-        return a / b;
+    public static void div(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Primeiro número: ");
+        double a = input.nextInt();
+        System.out.print("Segundo número: ");
+        double b = input.nextInt();
+        if(b == 0){
+            System.out.println("ERROR");
+        } else {
+            System.out.println("Divisão: " + (a/b));
+        }
     }
 
 }
